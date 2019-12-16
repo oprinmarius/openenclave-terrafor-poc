507af57991a642d02f15fc74948f7d0dc2b36e1b
brmclare <brmclare@microsoft.com>
2019-12-16 23:56:19 +0000

    add vmss slave

--- jenkins-agents/1804ACC.tf
+++ jenkins-agents/1804ACC.tf
@@ -1,34 +1,24 @@
+module "network" {
+  source              = "Azure/network/azurerm"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  vnet_name           = var.vnet_name
+  address_space       = var.address_space
+  subnet_prefixes     = var.subnet_prefixes
+  subnet_names        = var.subnet_names
+  tags                = var.tags
+}
+
 module "loadbalancer" {
   source              = "Azure/loadbalancer/azurerm"
   resource_group_name = "${var.resource_group_name}"
-  location            = "westus"
+  location            = "${var.location}"
   prefix              = "terraform-test"
-  lb_port             = {
-                          http  = ["80", "Tcp", "80"]
-                          https = ["443", "Tcp", "443"]
-                          #ssh   = ["22", "Tcp", "22"]
-                        }
-}
+  remote_port = {
+    ssh = ["Tcp", "22"]
+  }
 
-module "computegroup" {
-    source              = "Azure/computegroup/azurerm"
-    resource_group_name = "${var.resource_group_name}"
-    location            = "westus"
-    vm_size             = "Standard_A0"
-    admin_username      = "azureuser"
-    admin_password      = "ComplexPassword"
-    ssh_key             = "~/.ssh/id_rsa.pub"
-    nb_instance         = 2
-    vm_os_simple        = "UbuntuServer"
-    vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-    load_balancer_backend_address_pool_ids = "${module.loadbalancer.azurerm_lb_backend_address_pool_id}"
-    cmd_extension       = "sudo apt-get -y install nginx"
-    tags                = {
-                            environment = "dev"
-                            costcenter  = "it"
-                          }
-}
-
-output "vmss_id"{
-  value = "${module.computegroup.vmss_id}"
+  lb_port = {
+    http = ["80", "Tcp", "80"]
+  }
 }
--- jenkins-agents/variables.tf
+++ jenkins-agents/variables.tf
@@ -50,4 +50,5 @@ variable "vm_size" {
 
 variable "oeadmin_ssh_pub_key" {
   description = "Path to the public key to be used for ssh access to the VM.  Only used with non-Windows vms and can be left as-is even if using Windows vms. If specifying a path to a certification on a Windows machine to provision a linux vm use the / in the path versus backslash. e.g. c:/home/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
 }
