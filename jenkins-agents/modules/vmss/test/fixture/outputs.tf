0567422f45e91414b45894a6dc817289a0fd3672
brmclare <brmclare@microsoft.com>
2019-12-17 23:04:09 +0000

    module cutting

--- jenkins-agents/modules/vmss/test/fixture/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vmss_id" {
-  value = "${module.computegroup.vmss_id}"
-}
-
-output "public_ip_address" {
-  value = "${module.loadbalancer.azurerm_public_ip_address}"
-}
--- jenkins-agents/modules/vmss/test/fixture/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "location" {}
-variable "ssh_key" {}
-
-variable "resource_group_name" {
-  default = "terraform-test"
-}
-
-variable "vm_os_simple" {}
